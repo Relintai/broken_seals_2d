90a602a81f5dc104ffead25e505b9cb96e9c150e
Relintai <relintai@gmail.com>
2019-12-20 19:34:57 +0100

    License headers.

--- game/scripts/item_visuals/CharacterAtlasEntry2D.gd
+++ game/scripts/item_visuals/CharacterAtlasEntry2D.gd
@@ -1,6 +1,10 @@
 extends CharacterAtlasEntry
 class_name CharacterAtlasEntry2D
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 export(Rect2) var front_rect : Rect2
 export(Rect2) var back_rect : Rect2 
 export(Rect2) var right_rect : Rect2
