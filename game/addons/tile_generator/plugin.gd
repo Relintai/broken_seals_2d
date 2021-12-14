d24add3723ed6658d423e326aab836482d658a23
Relintai <relintai@protonmail.com>
2021-12-14 17:50:16 +0100

    Added a new shear tile resource.

--- game/addons/tile_generator/plugin.gd
+++ game/addons/tile_generator/plugin.gd
@@ -2,9 +2,12 @@ tool
 extends EditorPlugin
 
 var SimpleTile = preload("res://addons/tile_generator/smple_tile.gd")
+var ShearTile = preload("res://addons/tile_generator/shear_tile.gd")
 
 func _enter_tree():
 	add_custom_type("SimpleTile", "Resource", SimpleTile, null)
+	add_custom_type("ShearTile", "Resource", ShearTile, null)
 
 func _exit_tree():
 	remove_custom_type("SimpleTile")
+	remove_custom_type("ShearTile")
