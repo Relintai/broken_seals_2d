5799047a79750e4958623d8790fb27263e37f85c
Relintai <relintai@protonmail.com>
2022-02-20 23:55:27 +0100

    Updated the tools.

--- make_release.sh
+++ make_release.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+version=""
+version_snake_cased=""
+
+if [ ! -z $1 ]; then
+    version="."
+    version+=$1
+
+    version_snake_cased=${version//./_}
+fi
+
 project_root=$(pwd)
 
 rm -Rf ./release
@@ -9,30 +19,35 @@ mkdir release
 
 cd export
 
-rm -Rf broken_seals_full_source
-rm -Rf broken_seals_game_source
+rm -Rf broken_seals${version_snake_cased}_full_source
+rm -Rf broken_seals${version_snake_cased}_game_source
 
-mkdir broken_seals_full_source
-mkdir broken_seals_game_source
+mkdir broken_seals${version_snake_cased}_full_source
+mkdir broken_seals${version_snake_cased}_game_source
 
-python ../tools/copy_repos.py ../ ./broken_seals_full_source
-python ../tools/copy_repos.py ../game/ ./broken_seals_game_source
+# Warn if a file is over a megabyte. Used to catch big temporary files that would slip through outherwise
+python ../tools/copy_repos.py ../ ./broken_seals${version_snake_cased}_full_source 1048576
+python ../tools/copy_repos.py ../game/ ./broken_seals${version_snake_cased}_game_source
 
-zip  ../release/broken_seals_android_debug.zip  ./broken_seals_android_debug/*
-zip  ../release/broken_seals_android_release.zip  ./broken_seals_android_release/*
-zip  ../release/broken_seals_javascript.zip  ./broken_seals_javascript/*
-zip  ../release/broken_seals_linux.zip  ./broken_seals_linux/*
-zip  ../release/broken_seals_windows.zip  ./broken_seals_windows/*
-zip  ../release/broken_seals_pi4.zip  ./broken_seals_pi4/*
+zip -q ../release/broken_seals${version_snake_cased}_android_debug.zip  ./broken_seals${version_snake_cased}_android_debug/*
+zip -q ../release/broken_seals${version_snake_cased}_android_release.zip  ./broken_seals${version_snake_cased}_android_release/*
+zip -q ../release/broken_seals${version_snake_cased}_javascript.zip  ./broken_seals${version_snake_cased}_javascript/*
+zip -q ../release/broken_seals${version_snake_cased}_linux.zip  ./broken_seals${version_snake_cased}_linux/*
+zip -q ../release/broken_seals${version_snake_cased}_windows.zip  ./broken_seals${version_snake_cased}_windows/*
+zip -q ../release/broken_seals${version_snake_cased}_pi4.zip  ./broken_seals${version_snake_cased}_pi4/*
+zip -r -q ../release/broken_seals${version_snake_cased}_osx.zip  ./broken_seals${version_snake_cased}_osx/*
 
-zip  ../release/editor_windows.zip  ./godot.bs.windows.opt.tools.64.exe
-zip  ../release/editor_linux.zip  ./godot.bs.x11.opt.tools.64
-zip  ../release/editor_pi4.zip  ./godot.bs.x11.pi4.opt.tools.32
+# Editor
+zip -q ../release/editor_windows_bs${version_snake_cased}.zip  ./godot.bs${version}.windows.opt.tools.64.exe
+zip -q ../release/editor_linux_bs${version_snake_cased}.zip  ./godot.bs${version}.x11.opt.tools.64
+zip -q ../release/editor_pi4_bs${version_snake_cased}.zip  ./godot.bs${version}.x11.pi4.opt.tools.32
+cp  ./godot.bs${version}.javascript.opt.tools.zip ../release/editor_javascript_bs${version_snake_cased}.zip
+zip -q ../release/editor_osx_bs${version_snake_cased}.zip  ./godot.bs${version}.osx.opt.tools.zip
 
-zip  ../release/export_templates.zip  ./export_templates/*
+zip -q ../release/export_templates_bs${version_snake_cased}.zip  ./export_templates_bs${version_snake_cased}/*
 
-zip -r ../release/broken_seals_full_source.zip  ./broken_seals_full_source/*
-zip -r ../release/broken_seals_game_source.zip  ./broken_seals_game_source/*
+zip -q -r ../release/broken_seals${version_snake_cased}_full_source.zip  ./broken_seals${version_snake_cased}_full_source/*
+zip -q -r ../release/broken_seals${version_snake_cased}_game_source.zip  ./broken_seals${version_snake_cased}_game_source/*
 
 cd ..
 
