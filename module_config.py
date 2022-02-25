f8f427ef46cac37cfae14476403c04c868164916
Relintai <relintai@protonmail.com>
2022-02-26 00:08:59 +0100

    Added back in  Terraman2D, Props2D, MeshDataResource, and MeshUtils modules.

--- module_config.py
+++ module_config.py
@@ -9,15 +9,15 @@ module_repositories = [
     [ ['https://github.com/Relintai/thread_pool.git', 'git@github.com:Relintai/thread_pool.git'], 'thread_pool', '' ],
     [ ['https://github.com/Relintai/broken_seals_module.git', 'git@github.com:Relintai/broken_seals_module.git'], 'broken_seals_module', '' ],
     [ ['https://github.com/Relintai/rtile_map.git', 'git@github.com:Relintai/rtile_map.git'], 'rtile_map', '' ],
+    [ ['https://github.com/Relintai/mesh_data_resource.git', 'git@github.com:Relintai/mesh_data_resource.git'], 'mesh_data_resource', '' ],
+    [ ['https://github.com/Relintai/mesh_utils.git', 'git@github.com:Relintai/mesh_utils.git'], 'mesh_utils', '' ],
+    [ ['https://github.com/Relintai/props_2d.git', 'git@github.com:Relintai/props_2d.git'], 'props_2d', '' ],
+    [ ['https://github.com/Relintai/terraman_2d.git', 'git@github.com:Relintai/terraman_2d.git'], 'terraman_2d', '' ],
 ]
 
 removed_modules = [
     [ ['https://github.com/Relintai/world_generator.git', 'git@github.com:Relintai/world_generator.git'], 'world_generator', '' ],
-    [ ['https://github.com/Relintai/terraman_2d.git', 'git@github.com:Relintai/terraman_2d.git'], 'terraman_2d', '' ],
     [ ['https://github.com/Relintai/props.git', 'git@github.com:Relintai/props.git'], 'props', '' ],
-    [ ['https://github.com/Relintai/props_2d.git', 'git@github.com:Relintai/props_2d.git'], 'props_2d', '' ],
-    [ ['https://github.com/Relintai/mesh_data_resource.git', 'git@github.com:Relintai/mesh_data_resource.git'], 'mesh_data_resource', '' ],
-    [ ['https://github.com/Relintai/mesh_utils.git', 'git@github.com:Relintai/mesh_utils.git'], 'mesh_utils', '' ],
 ]
 
 addon_repositories = [
